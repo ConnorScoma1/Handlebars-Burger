a2dbca744164f695be0d97f83985df7f2582a55f
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-07-09 13:04:56 -0500

    adding server connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+
+connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burger_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.log("error " + err.stack);
+  }
+
+  console.log("connected on thread id: " + connection.treadId);
+});
---
+++ config/orm.js
@@ -0,0 +1,34 @@
+var connection = require("../config/connection");
+
+var orm = {
+  selectAll: function(table, cb) {
+    var dbQuery = "SELECT * FROM " + table + ";";
+
+    connection.query(dbQuery, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cd(res);
+    });
+  },
+
+  insertOne: function(table, cols, cals, cb) {
+    var dbQuery =
+      "INSERT INTO " +
+      table +
+      " (" +
+      cols.toString() +
+      ")" +
+      "VALUES (" +
+      createQmarks(vals.length) +
+      ") ";
+
+    console.log(dbQuery);
+    connection.query(dbQuery, vals, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cd(res);
+    });
+  }
+};
