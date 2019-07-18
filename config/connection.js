9a93c31e2909245bedd515a02ad97877ab933753
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-07-18 15:47:05 -0500

    adding jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
 
-connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "54377Sco",
-  database: "burger_db"
-});
+// mysql://czbds431umi5x7wo:anjuim08apizt3ra@erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/njfs3dmontnydbrk
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+    host: "erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "czbds431umi5x7wo",
+    password: "anjuim08apizt3ra",
+    port: 3306,
+    database: "njfs3dmontnydbrk"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
