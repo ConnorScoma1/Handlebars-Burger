f0c766169a5caaa5148fb5f9b2488ad29daa2f13
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-07-18 15:47:44 -0500

    adding heroku

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,6 @@ var mysql = require("mysql");
 
 // mysql://czbds431umi5x7wo:anjuim08apizt3ra@erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/njfs3dmontnydbrk
 
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
     connection = mysql.createConnection({
     host: "erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "czbds431umi5x7wo",
@@ -12,7 +9,7 @@ if(process.env.JAWSDB_URL) {
     port: 3306,
     database: "njfs3dmontnydbrk"
   });
-}
+
 
 connection.connect(function(err) {
   if (err) {
